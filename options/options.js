840a375c250952392a7b3be423ab5249b0f98e46
Crowdbotics <engineering@crowdbotics.com>
2023-07-10 12:12:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "young-credit-dev-99509",
+  url: "https://young-credit-dev-99509.botics.co",
+  api: "https://young-credit-dev-99509.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
